a8a4c9f3d8d7b6c37d5dc71f0b1126437dff5eea
Fabiano Spinelli <spinefa@ies.jrc.it>
2021-04-30 11:31:38 +0200

    Initial Commit

---
+++ js/config.js
@@ -0,0 +1,7 @@
+//var serverURL = "https://inspire-sandbox.jrc.ec.europa.eu/validator-testbed/rest/";
+//var serverRealURL = "https://inspire.ec.europa.eu/validator/v2/";
+//var serverCaptchaURL = "https://inspire-sandbox.jrc.ec.europa.eu/validator-testbed/captcha/verify.php";
+var serverURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk.com/etf-webapp/v2/";
+var serverRealURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk.com/etf-webapp/v2/";
+var serverCaptchaURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk.com/etf-webapp/captcha/verify.php";
+
