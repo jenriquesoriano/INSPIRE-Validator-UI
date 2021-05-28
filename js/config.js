e66333729e9fc1f5c5e0b9da54c103f6ae794868
Carlos Palma <carlospalma@guadaltel.com>
2021-05-28 10:49:41 +0200

    Modified config.js URLs to staging instance

--- js/config.js
+++ js/config.js
@@ -3,9 +3,9 @@
 //var serverRealURL = "https://inspire.ec.europa.eu/validator/v2/";
 //var serverCaptchaURL = "https://inspire-sandbox.jrc.ec.europa.eu/validator-testbed/captcha/verify.php";
 // SANDBOX
-var serverURL = "https://inspire-sandbox.jrc.ec.europa.eu/validator-testbed/rest/";
+var serverURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk.com/validator/v2/";
 var serverRealURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk.com/validator/v2/";
-var serverCaptchaURL = "https://inspire-sandbox.jrc.ec.europa.eu/validator-testbed/captcha/verify.php";
+var serverCaptchaURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk.com/etf-webapp/v2/";
 // STAGING
 //var serverURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk.com/validator/v2/";
 //var serverRealURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk.com/validator/v2/";
