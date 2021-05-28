7b76e938555f1500a47b236749b04741584b74dc
Carlos Palma <carlospalma@guadaltel.com>
2021-05-28 11:38:12 +0200

    fixed CAPTCHA URL on config.js

--- js/config.js
+++ js/config.js
@@ -3,11 +3,11 @@
 //var serverRealURL = "https://inspire.ec.europa.eu/validator/v2/";
 //var serverCaptchaURL = "https://inspire-sandbox.jrc.ec.europa.eu/validator-testbed/captcha/verify.php";
 // SANDBOX
+// var serverURL = "https://inspire-sandbox.jrc.ec.europa.eu/validator-testbed/rest/";
+// var serverRealURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk.com/validator/v2/";
+// var serverCaptchaURL = "https://inspire-sandbox.jrc.ec.europa.eu/validator-testbed/captcha/verify.php";
+// STAGING
 var serverURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk.com/validator/v2/";
 var serverRealURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk.com/validator/v2/";
-var serverCaptchaURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk.com/etf-webapp/v2/";
-// STAGING
-//var serverURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk.com/validator/v2/";
-//var serverRealURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk.com/validator/v2/";
-//var serverCaptchaURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk.com/validator/captcha/verify.php";
+var serverCaptchaURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk.com/etf-webapp/captcha/verify.php";
 
