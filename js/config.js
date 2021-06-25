25977298677a0ea06a00067350f2d25f43c80f4e
jenriquesoriano <jenriquesoriano@guadaltel.com>
2021-06-25 13:50:55 +0200

    Updated serverCaptchaURL parameter for Staging deployment
    Updated serverCaptchaURL parameter for Staging deployment

--- js/config.js
+++ js/config.js
@@ -12,5 +12,5 @@ var labelStaging = true;
 // STAGING
 var serverURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk.com/validator/v2/";
 var serverRealURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk.com/validator/v2/";
-var serverCaptchaURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk.com/etf-webapp/captcha/verify.php";
+var serverCaptchaURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk.com/validator/captcha/verify.php";
 
