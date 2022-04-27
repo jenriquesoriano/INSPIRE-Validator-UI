4af12cec6b93ba202f13b0f0ce08f82a23244b28
jenriquesoriano <jenriquesoriano@guadaltel.com>
2022-04-27 09:38:01 +0200

    Revert "Updated files to parametrize some links"

--- js/config.js
+++ js/config.js
@@ -8,6 +8,3 @@ var serverRealURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk
 var serverDirectURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk.com/validator/v2/";
 var serverCaptchaURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk.com/validator/captcha/verify.php";
 var serverToken = "";
-var swaggerURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk.com/validator/swagger-ui.html"
-// Production
-//var swaggerURL = "https://inspire.ec.europa.eu/validator/swagger-ui.html"
