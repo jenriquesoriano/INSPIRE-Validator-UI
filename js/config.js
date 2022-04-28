a77a06508565bea84efc3c373fc769c12019dedf
manuelaromero-guadaltel <manuelaromero@guadaltel.es>
2022-04-28 13:12:08 +0200

    Update to parameterize

--- js/config.js
+++ js/config.js
@@ -8,3 +8,6 @@ var serverRealURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk
 var serverDirectURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk.com/validator/v2/";
 var serverCaptchaURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk.com/validator/captcha/verify.php";
 var serverToken = "";
+var swaggerURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk.com/validator/swagger-ui.html"
+// Production
+//var swaggerURL = "https://inspire.ec.europa.eu/validator/swagger-ui.html"
