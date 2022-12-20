e6b28c11db1ff917091a7d0a4a3b37d94cd8c1ca
manuelaromero-guadaltel <manuelaromero@guadaltel.es>
2022-12-20 09:53:27 +0100

    Option 2 corrected
    Signed-off-by: manuelaromero-guadaltel <manuelaromero@guadaltel.es>

--- js/results/controller.js
+++ js/results/controller.js
@@ -1066,7 +1066,6 @@ ngApp.controller('myValidatorController', function($scope) {
 					if (Array.isArray(item.testTasks.TestTask) == false) {
 						var resourceType = item.testTasks.TestTask.executableTestSuite.href;
 						resourceType = resourceType.replace("//v2", "/v2");
-						resourceType = resourceType.replace("https", "http");
 						resourceType = resourceType.replace($scope.urlValidator + "ExecutableTestSuites/", "");
 						resourceType = resourceType.replace(".json", "");
 						resourceType = "EID" + resourceType;
@@ -1076,7 +1075,6 @@ ngApp.controller('myValidatorController', function($scope) {
 						for (var i = 0; i < numTestSuites; i++) {
 							var resourceType = item.testTasks.TestTask[i].executableTestSuite.href;
 							resourceType = resourceType.replace("//v2", "/v2");
-							resourceType = resourceType.replace("https", "http");
 							resourceType = resourceType.replace($scope.urlValidator + "ExecutableTestSuites/", "");
 							resourceType = resourceType.replace(".json", "");
 							resourceType = "EID" + resourceType;
